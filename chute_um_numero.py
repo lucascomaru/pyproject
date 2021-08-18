fd07bf6d0f0b3fd1bea0f79266910dcaa60156a6
Lucas <lucascomaru20@gmail.com>
2021-08-17 21:39:54 -0300

    Gerando valor aleatorio

--- chute_um_numero.py
+++ chute_um_numero.py
@@ -5,14 +5,27 @@ class ChuteUmNumero:
         self.valor_aleatorio = 0
         self.valor_minimo = 1
         self.valor_maximo = 100
+        self.tentar_novamente = True
 
     def Iniciar(self):
-        self.GerarUmNumeroAleatorio()
-        valor_do_chute = input('Chute um número: ')
-        if int(valor_do_chute) > self.valor_aleatorio:
-            print('Chute um valor mais baixo')
-        elif int(valor_do_chute) < self.valor_aleatorio:
-            print('Chute um valor mais alto')
+        self.GerarNumeroAleatorio()
+        self.PedirValorAleatorio()
+        while self.tentar_novamente == True:
+            if int(self.valor_do_chute) > self.valor_aleatorio:
+                print('Chute um valor mais baixo')
+                self.PedirValorAleatorio()
+            elif int(self.valor_do_chute) < self.valor_aleatorio:
+                print('Chute um valor mais alto')
+                self.PedirValorAleatorio()
+            if int(self.valor_do_chute) == self.valor_aleatorio:
+                self.tentar_novamente = False
+                print('Parabéns, você acertou!! ')
 
     def PedirValorAleatorio(self):
         self.valor_do_chute = input('Chute um número: ')
+
+    def GerarNumeroAleatorio(self):
+        self.valor_aleatorio = random.randint(self.valor_minimo, self.valor_maximo)
+
+chute = ChuteUmNumero()
+chute.Iniciar()
